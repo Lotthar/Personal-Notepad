63cf395bcac8100c9ccb25766fe809998f3f3b32
lothar- <branko.rovcanin@gmail.com>
2019-03-26 13:59:04 +0100

    added option menu, added button events(newPad,Save,Saved Pads)

--- js/main.js
+++ js/main.js
@@ -3,8 +3,26 @@ $(document).ready(function() {
     let $prozorVisina = $(window).height() - 90;
     $('.notepad').height($prozorVisina - 20);
     $(window).resize(function(){
-        let $prozorVisina = $(window).height() - 90;
+        let $prozorVisina = $(window).height() - 135;
         $('.notepad').height($prozorVisina - 20);
     });
 });
 let btn = [];
+// pojavljivanje pada za cuvanje na klik na dugme "saved pads"
+$("#savedPads").click(function () {
+    $("#text-wrap").toggleClass(" col-md-8");
+    $("#savedNotes").toggleClass(" col-md-4 on-off");
+});
+// Stavljanje novog pada
+$("#newPad").click(function(){
+    let text = document.getElementById("textArea").value;
+    if(text === "") saveNotepad(); // funckija koja ce da vraca cuva notepad
+    else{
+        if(confirm("Are you sure you don't want to save this Pad?")){
+            document.getElementById("textArea").value = "";
+        }
+        else{
+            return;
+        }
+    }
+});
